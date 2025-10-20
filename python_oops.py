89e83a28e7ca9f0a0f8f6793f8b4505c2de55b67
Logesh-Pro <logeshb451@gmail.com>
2025-10-20 13:30:44 +0530

    commit check

--- python_oops.py
+++ python_oops.py
@@ -1,1 +1,11 @@
 # oops using python
+class Item:
+    pass
+item1 = Item()
+item1.name = "phone"
+item1.price = 100
+item1.quantity = 5
+print(type(item1))
+print(type(item1.name))
+print(type(item1.price))
+print(type(item1.quantity))
