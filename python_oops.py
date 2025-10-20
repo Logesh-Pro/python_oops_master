bfd6fe96c176a4a8a150cf1e7da43e7005e5a32b
Logesh-Pro <logeshb451@gmail.com>
2025-10-21 00:38:43 +0530

    __init__ and self explanation

--- python_oops.py
+++ python_oops.py
@@ -1,6 +1,26 @@
 class computer:
-    pass
+    def __init__(self):
+        self.name="logesh"
+        self.age=18
+    def update(self):
+        self.age=30  
+    def compare(self,other):
+        if self.age==other.age:
+            return True
+        else:
+            return False      
 c1=computer() #constuctor called internully when object created
 c2=computer()
-print(id(c1)) 
-print(id(c2))  # every time a new object created a new memory allocated for that object.
+if c1.compare(c2):
+    print("They are same")
+c1.name="kumar"
+c1.age=20
+c1.update()
+if c1.compare(c2):
+    print("They are same")
+else:
+    print("They are different")    
+print(c1.name)
+print(c1.age) 
+print(c2.name)
+print(c2.age)
