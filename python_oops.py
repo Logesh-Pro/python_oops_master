a99387d747ae1fbe3e99506c3e8ce4567f9c065c
Logesh-Pro <logeshb451@gmail.com>
2025-10-20 13:44:33 +0530

    commit check

--- python_oops.py
+++ python_oops.py
@@ -1,11 +1,19 @@
-# oops using python
-class Item:
-    pass
-item1 = Item()
-item1.name = "phone"
-item1.price = 100
-item1.quantity = 5
-print(type(item1))
-print(type(item1.name))
-print(type(item1.price))
-print(type(item1.quantity))
+class Dog:
+    def __init__(self,name,breed,owner):
+        self.name = name
+        self.breed = breed
+        self.owner = owner
+    def bark(self):
+        print("whoof")
+class Owner:
+    def __init__(self,name,address,contact_number):
+        self.name = name
+        self.address = address
+        self.phone_number = contact_number
+owner1= Owner("John Doe","123 Street","555-1234")
+dog1 = Dog("Buddy","Labrador",owner1)
+dog1.owner.name
+print(dog1.owner.name)  
+owner2= Owner("Jane Smith","456 Avenue","555-5678")
+dog2 = Dog("Max","Beagle",owner2)
+print(dog2.owner.name)
