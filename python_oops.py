a13aa5a3ffb083d91798f58e29bfb57a70cfa4c2
Logesh-Pro <logeshb451@gmail.com>
2025-10-21 01:00:02 +0530

    accessor and mutator methods

--- python_oops.py
+++ python_oops.py
@@ -1,11 +1,17 @@
-class car:
-    wheel = 4 # class variable or static variable
-    def __init__(self):
-        self.mil = 10 # instance variable or name space
-        self.com = "BMW"
-c1 = car()
-c2 = car()
-c1.mil = 8
-car.wheel = 5 #changing in all objects
-print(c1.com, c1.mil, c1.wheel)
-print(c2.com, c2.mil, c2.wheel)
+# meaning of instance is it belong to a particular object
+#there are two types of instance methods accessor and mutator methods
+class student:
+    school = "Telusko"
+    def __init__(self,m1,m2,m3):
+        self.m2=m2
+        self.m3=m3
+    def avg(self):
+        return (self.m1+self.m2+self.m3)/3   
+    def get_m1(self): #accessor method (only fetch the value)
+        return self.m1
+    def set_m1(self,value): #mutator method (modify the value)
+        self.m1=value 
+s1 = student(34,56,78)
+s2 = student(45,67,89)
+print(s1.avg())
+print(s2.avg())
