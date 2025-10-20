c72670b0fa97e3013595ba466815d7dcba2ee090
Logesh-Pro <logeshb451@gmail.com>
2025-10-20 14:21:55 +0530

    __init__ and self explanation

--- python_oops.py
+++ python_oops.py
@@ -1,11 +1,14 @@
 #telesco
 #in python all are objects
+# as of now self is for reseaving a object passed
 class computer:
+    def __init__(self,cpu,ram): # by defult it will call
+        self.cpu= cpu  #self is used to refering the object
+        self.ram= ram 
     def config(self): #self is objrct we passing
-        print("i5, 16gb, 512GB")
-com1=computer() #initalizing a object
-computer.config(com1) #we want say which object we are using
-com2=computer()
-computer.config(com2) #more clear way of calling a method in class
+        print("Config is ", self.cpu,self.ram)
+com1=computer('i5',16) #initalizing a object
+com2=computer('Ryzen 3',8)
 com1.config() #more simple way to call a method in class
 com2.config() #config take the object before as object
+# we are binding every data with a  method with respective(object) self
