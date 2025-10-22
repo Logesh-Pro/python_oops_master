cf5a052fbaf128c9f004567540e032ec310872b8
Logesh-Pro <logeshb451@gmail.com>
2025-10-22 20:47:19 +0530

    object calling of inner class

--- python_oops.py
+++ python_oops.py
@@ -1,3 +1,4 @@
+#you can create object of inner class inside the outer class or you can can create object of inner class outside the outer class provided you use outer class name to call it
 class student: #outer class
     def __init__(self,name,rollno):
         self.name = name
@@ -5,15 +6,18 @@ class student: #outer class
         self.lap = self.laptop()
     def show(self):
         print(self.name,self.roll_no)
+        self.lap.show()
     class laptop: #inner class
         def __init__(self):
             self.brand='hp'
             self.cpu='i5'
             self.ram='8'
+        def show(self):
+            print(self.brand,self.cpu,self.ram)    
 s1=student("Alice",101)
 s2=student("Bob",102)
 s1.show()       
-lap1=s1.lap #calling with outer class object
-lap2=s2.lap
+lap1=student.laptop()
+lap2=student.laptop()
 print(id(lap1))
 print(id(lap2))
