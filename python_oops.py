86af569357a04130d20dbb4ad5bfcb93e5103e00
Logesh-Pro <logeshb451@gmail.com>
2025-10-22 23:57:47 +0530

    method overriding

--- python_oops.py
+++ python_oops.py
@@ -1,15 +1,8 @@
-class Student:
-    def __init__(self,m1,m2):
-        self.m1=m1
-        self.m2=m2
-    def sum(self,a=None,b=None,c=None):
-        if a!=None and b!=None and c!=None:
-            s=a+b+c
-        elif a!=None and b!=None:
-            s=a+b
-        else:
-            s=a
-        return s    
-s1=Student(58,69)
-print(s1.sum(5))
-#method overlading is not possible in python but we can achive by some tricks
+class a:
+    def show(self):
+        print("in a show")
+class b(a):
+    def show(self):
+        print("in b show")        
+a1=b()
+a1.show()        
