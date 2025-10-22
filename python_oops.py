a5d82496d56eed69256c8b3454c1f8f8b3304ff2
Logesh-Pro <logeshb451@gmail.com>
2025-10-22 23:26:12 +0530

    polymorphism duck type

--- python_oops.py
+++ python_oops.py
@@ -1,22 +1,18 @@
-class a:
-    def __init__(self):
-        print("in a init")
-    def feature1(self):
-        print("feature 1-a working")
-    def feature2(self):
-        print("feature 2 working")
-class b:
-    def __init__(self):
-        print("in b init")
-    def feature1(self):
-        print("feature 1-b working")
-    def feature4(self):
-        print("feature 4 working")  
-class c(a,b):
-    def __init__(self):
-        super().__init__() #method resolution order(MRO)(left to right)(so c(a,b) a comes first so a)
-        print("in c init")     
-    def feat(self):
-        super.feature2()#calling feature2 of class a using super() (to reperesent a super class method we can use super keyword)
-a1 = c() 
-a1.feature1()#(MRO will work on methods also)
+#polymorphism
+class pycharm:
+    def execute(self):
+        print("compiling")
+        print("running")
+class vscode:
+    def execute(self):
+        print("spell check")
+        print("convention check")
+        print("compiling")
+        print("running")        
+class laptop:
+    def code(self, ide):
+        ide.execute()
+ide=pycharm()
+lap1 = laptop()
+lap1.code(ide)    
+# if there is a method i no need which class it is we a that method this is polymorphism duck typing(walk like a duck, swim like a duck simplily it is a duck)    
