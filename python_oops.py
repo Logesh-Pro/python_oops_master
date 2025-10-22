990b446033f53ab9d15178189e65258553a6fed1
Logesh-Pro <logeshb451@gmail.com>
2025-10-22 23:51:47 +0530

    method overlading

--- python_oops.py
+++ python_oops.py
@@ -1,28 +1,15 @@
-#operator overloading
 class Student:
     def __init__(self,m1,m2):
         self.m1=m1
         self.m2=m2
-    def __add__(self,other):    
-        m1=self.m1+other.m1
-        m2=self.m2+other.m2
-        s3=Student(m1,m2)
-        return s3
-    def __gt__(self,other):
-        r1=self.m1+self.m2
-        r2=other.m1+other.m2
-        if r1>r2:
-            return True
+    def sum(self,a=None,b=None,c=None):
+        if a!=None and b!=None and c!=None:
+            s=a+b+c
+        elif a!=None and b!=None:
+            s=a+b
         else:
-            return False
-    def __str__(self):  
-        return self.m1,self.m2  
+            s=a
+        return s    
 s1=Student(58,69)
-s2=Student(78,89)        
-s3=s1+s2 #->student.__add__(s1,s2)
-if(s1>s2):
-    print("s1 wins")
-else:   
-    print("s2 wins")
-print(s3.m1)
-print(s1.__str__())
+print(s1.sum(5))
+#method overlading is not possible in python but we can achive by some tricks
