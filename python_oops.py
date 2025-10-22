1006dd6146603919bc4ba6b94f3f1566d366aad3
Logesh-Pro <logeshb451@gmail.com>
2025-10-22 21:05:42 +0530

    inheretance constructor type one

--- python_oops.py
+++ python_oops.py
@@ -1,20 +1,14 @@
 class a:
+    def __init__(self):
+        print("in a init")
     def feature1(self):
         print("feature 1 working")
     def feature2(self):
         print("feature 2 working")
-class b:
+class b(a):
     def feature3(self):
         print("feature 3 working")
     def feature4(self):
-        print("feature 4 working")  
-class c(a,b):#multiple inheritance (using class b and a methods since c is child class of b)
-    def feature5(self):
-        print("feature 5 working")         
-a1=a()
-a1.feature1()
-a1.feature2()     
-b1=b()
-c1=c()
-c1.feature1()
-c1.feature3()
+        print("feature 4 working")       
+a1 = b() 
+#sub class can call super class init if it not have init on its class
