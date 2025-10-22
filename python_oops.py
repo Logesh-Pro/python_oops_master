e7979576074c238ca9baa151445b4fef66de07f1
Logesh-Pro <logeshb451@gmail.com>
2025-10-22 21:15:22 +0530

    MRO on methods

--- python_oops.py
+++ python_oops.py
@@ -2,14 +2,14 @@ class a:
     def __init__(self):
         print("in a init")
     def feature1(self):
-        print("feature 1 working")
+        print("feature 1-a working")
     def feature2(self):
         print("feature 2 working")
 class b:
     def __init__(self):
         print("in b init")
-    def feature3(self):
-        print("feature 3 working")
+    def feature1(self):
+        print("feature 1-b working")
     def feature4(self):
         print("feature 4 working")  
 class c(a,b):
@@ -17,3 +17,4 @@ class c(a,b):
         super().__init__() #method resolution order(MRO)(left to right)(so c(a,b) a comes first so a)
         print("in c init")           
 a1 = c() 
+a1.feature1()#(MRO will work on methods also)
