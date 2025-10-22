dc0ce93cef40010abafa4c1d740ceedcdec275c1
Logesh-Pro <logeshb451@gmail.com>
2025-10-22 23:42:31 +0530

    method overloading

--- python_oops.py
+++ python_oops.py
@@ -1,18 +1,29 @@
-#polymorphism
-class pycharm:
-    def execute(self):
-        print("compiling")
-        print("running")
-class vscode:
-    def execute(self):
-        print("spell check")
-        print("convention check")
-        print("compiling")
-        print("running")        
-class laptop:
-    def code(self, ide):
-        ide.execute()
-ide=pycharm()
-lap1 = laptop()
-lap1.code(ide)    
-# if there is a method i no need which class it is we a that method this is polymorphism duck typing(walk like a duck, swim like a duck simplily it is a duck)    
+#operator overloading
+class Student:
+    def __init__(self,m1,m2):
+        self.m1=m1
+        self.m2=m2
+    def __add__(self,other):    
+        m1=self.m1+other.m1
+        m2=self.m2+other.m2
+        s3=Student(m1,m2)
+        return s3
+    def __gt__(self,other):
+        r1=self.m1+self.m2
+        r2=other.m1+other.m2
+        if r1>r2:
+            return True
+        else:
+            return False
+    def __str__(self):  
+        return f"{self.m1} {self.m2}"  
+s1=Student(58,69)
+s2=Student(78,89)        
+s3=s1+s2 #->student.__add__(s1,s2)
+if(s1>s2):
+    print("s1 wins")
+else:   
+    print("s2 wins")
+print(s3.m1)
+print(s1)
+#redefine the rule is known as method overloading (eg:we can do addion by '-' symbol)
